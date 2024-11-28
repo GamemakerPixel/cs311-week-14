2e016327f1bc9edab792e1d828d0692db4f88813
Aidan Bensch <57157106+GamemakerPixel@users.noreply.github.com>
2024-11-28 18:32:14 -0500

    Create initial application

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { useState, useRef } from "react"
+
+
+export default function Home() {
+  const initialInputText = "Enter a number to begin!"
+  const [userInput, setUserInput] = useState<string>(initialInputText);
+  const numberInput = useRef<InputHTMLElement>()
+
+  const labelClass = "flex flex-col text-center my-4"
+  const inputClass = "bg-background mx-auto border-b-2"
+  return (
+    <div>
+      <label className={labelClass}>
+	Your number is:
+	<input value={userInput} readOnly={true} className={`${inputClass} text-center`}/>
+      </label>
+      <label className={labelClass}>
+	Your number:
+	<input placeholder="1, 2, 3..." type="number" ref={numberInput} className={inputClass}/>
+      </label>
+      <button
+	className="mx-auto my-4 border-2 px-4 py-2 rounded-lg block"
+	onClick={() => {
+	  setUserInput(numberInput.current.value)
+	}}
+      >
+	Submit
+      </button>
+    </div>
+  )
+}
